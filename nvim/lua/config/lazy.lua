97c71ddb6f9b9c44a9d4f58f8d1bda64e302d731
Alexander Richey <alexander.richey@gmail.com>
2024-09-05 15:47:33 +0000

    fix tmux conf

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,3 +1,6 @@
+-- Lazy Setup
+-------------------------------------------------------------------------------
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
